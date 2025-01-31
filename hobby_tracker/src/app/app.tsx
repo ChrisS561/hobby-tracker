74d24edb306176cf8a9cef5e0367a073733a1300
Chris Smith Jr <Chrissmithjr97@gmail.com>
2025-01-31 14:32:29 -0500

    set the start of the project

---
+++ hobby_tracker/src/app/app.tsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <main>
+      
+      
+    
+      <footer className="pb-16 max-w-screen-lg xl:max-w-screen-xl mx-auto text-center sm:text-right text-gray-400 font-bold">
+        <a href="https://github.com/chrisS561">
+          Chris Smith @ {new Date().getFullYear()}
+        </a>
+      </footer>
+    </main>
+  );
+}
+
+export default App;
